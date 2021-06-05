8b17d9f9711622da72a523edf1d56fc98d54a2cf
Mark Julius Mella <46963478+Markmella@users.noreply.github.com>
2021-06-06 00:13:38 +0800

    edit Admin Controller

--- app/Http/Controllers/Admin/adminProfileController.php
+++ app/Http/Controllers/Admin/adminProfileController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Hash;
 
 class adminProfileController extends Controller
 {
+
+    
+
+
+
     public function store(Request $request){
         
         Admin::create([
